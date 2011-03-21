d56ee7cf13e912639e1cd6e57423f64e50707ddf
Devin Gaffney <itsme@devingaffney.com>
2011-03-20 17:10:55 -0700

    small fixes

--- slave/environment.rb
+++ slave/environment.rb
@@ -4,51 +4,53 @@ require 'rubygems'
 require 'bundler/setup'
 require 'dm-core'
 require 'dm-aggregates'
-require 'dm-chunked_query'
 require 'dm-validations'
 require 'dm-migrations'
 require 'dm-migrations/migration_runner'
-
-require File.dirname(__FILE__)+'/extensions/dm-extensions'
-require File.dirname(__FILE__)+'/extensions/array'
-require File.dirname(__FILE__)+'/extensions/string'
-require File.dirname(__FILE__)+'/extensions/hash'
-require File.dirname(__FILE__)+'/extensions/time'
-require File.dirname(__FILE__)+'/extensions/date'
-require File.dirname(__FILE__)+'/extensions/date_time'
-require File.dirname(__FILE__)+'/extensions/inflectors'
-
-require File.dirname(__FILE__)+'/utils/git'
-require File.dirname(__FILE__)+'/utils/sh'
-
-require File.dirname(__FILE__)+'/models/analysis_metadata'
-require File.dirname(__FILE__)+'/models/analytical_offering'
-require File.dirname(__FILE__)+'/models/analytical_offering_variable'
-require File.dirname(__FILE__)+'/models/analytical_offering_variable_descriptor'
-require File.dirname(__FILE__)+'/models/auth_user'
-require File.dirname(__FILE__)+'/models/curation'
-require File.dirname(__FILE__)+'/models/dataset'
-require File.dirname(__FILE__)+'/models/edge'
-require File.dirname(__FILE__)+'/models/entity'
-require File.dirname(__FILE__)+'/models/graph'
-require File.dirname(__FILE__)+'/models/graph_point'
-require File.dirname(__FILE__)+'/models/instance'
-require File.dirname(__FILE__)+'/models/lock'
-require File.dirname(__FILE__)+'/models/researcher'
-require File.dirname(__FILE__)+'/models/tweet'
-require File.dirname(__FILE__)+'/models/user'
-require File.dirname(__FILE__)+'/models/whitelisting'
-
-
-require File.dirname(__FILE__)+'/utils/tweet_helper'
-require File.dirname(__FILE__)+'/utils/entity_helper'
-
-require File.dirname(__FILE__)+'/utils/u'
-require File.dirname(__FILE__)+'/lib/tweetstream'
 require 'eventmachine'
 require 'em-http'
 require 'json'
 require 'twitter'
+
+DIR = File.dirname(__FILE__)
+
+require DIR+'/extensions/dm-extensions'
+require DIR+'/extensions/array'
+require DIR+'/extensions/string'
+require DIR+'/extensions/hash'
+require DIR+'/extensions/time'
+require DIR+'/extensions/date'
+require DIR+'/extensions/date_time'
+require DIR+'/extensions/inflectors'
+
+require DIR+'/utils/git'
+require DIR+'/utils/sh'
+
+require DIR+'/model'
+require DIR+'/models/analysis_metadata'
+require DIR+'/models/analytical_offering'
+require DIR+'/models/analytical_offering_variable'
+require DIR+'/models/analytical_offering_variable_descriptor'
+require DIR+'/models/auth_user'
+require DIR+'/models/curation'
+require DIR+'/models/dataset'
+require DIR+'/models/edge'
+require DIR+'/models/entity'
+require DIR+'/models/graph'
+require DIR+'/models/graph_point'
+require DIR+'/models/instance'
+require DIR+'/models/lock'
+require DIR+'/models/researcher'
+require DIR+'/models/tweet'
+require DIR+'/models/user'
+require DIR+'/models/whitelisting'
+
+
+require DIR+'/utils/tweet_helper'
+require DIR+'/utils/entity_helper'
+
+require DIR+'/utils/u'
+require DIR+'/lib/tweetstream'
 
 env = ENV["e"] || "development"
 db = YAML.load(File.read(ENV['PWD']+'/config/database.yml'))
@@ -59,8 +61,8 @@ db = db[env]
 DataMapper.finalize
 DataMapper.setup(:default, "#{db["adapter"]}://#{db["username"]}:#{db["password"]}@#{db["host"]}/#{db["database"]}")
 
-require File.dirname(__FILE__)+'/analyzer/analysis'
+require DIR+'/analyzer/analysis'
 
 Twit = Twitter::Client.new
 
-ROOT = File.dirname(__FILE__)
+ROOT = DIR
