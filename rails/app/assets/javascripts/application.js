0ce60355b611aff91e05886db937b6435bdd4ab4
Ian Pearce <peeinears@gmail.com>
2012-03-10 16:26:06 -0500

    messin around with some styles

--- rails/app/assets/javascripts/application.js
+++ rails/app/assets/javascripts/application.js
@@ -12,5 +12,18 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+// require twitter/bootstrap
+// require twitter/bootstrap
+//= require twitter/bootstrap/alert
+//= require twitter/bootstrap/button
+// require twitter/bootstrap/carousel
+// require twitter/bootstrap/collapse
+//= require twitter/bootstrap/dropdown
+// require twitter/bootstrap/modal
+// require twitter/bootstrap/popover
+// require twitter/bootstrap/scrollspy
+// require twitter/bootstrap/tab
+// require twitter/bootstrap/tooltip
+// require twitter/bootstrap/transition
+// require twitter/bootstrap/typeahead
 // require_tree .
