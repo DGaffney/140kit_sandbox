8a21a78a943522c16474e9d5c165ac93bae162de
Devin Gaffney <itsme@devingaffney.com>
2012-03-20 17:54:39 +0000

    woo

--- code/extensions/array.rb
+++ code/extensions/array.rb
@@ -23,26 +23,12 @@ class Array
       hash
     end
   end
-
-  def percentile(percentage=0.0)
-    another_array = self.to_a.dup
-    another_array.push(-1.0/0.0)                   # add -Infinity to be 0th index
-    another_array.sort!
-    another_array_size = another_array.size - 1    # disregard -Infinity
-    r = percentage.to_f * (another_array_size - 1) + 1
-    if r <= 1 then return another_array[1]
-    elsif r >= another_array_size then return another_array[another_array_size]
+  
+	def percentile(percentile=0.0)
+	  if percentile == 0.0
+	    return self.sort.first
+    else
+      return self ? self.sort[((self.length * percentile).ceil)-1] : nil
     end
-    ir = r.truncate
-    fr = fraction? r
-    another_array[ir] + fr*(another_array[ir+1] - another_array[ir])
   end
-
-  # def percentile(percentile=0.0)
-  #   if percentile == 0.0
-  #     return self.first
-  #     else
-  #       return self ? self.sort[((self.length * percentile).ceil)-1] : nil
-  #     end
-  #   end
 end
