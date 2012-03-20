653453c297969b6b7f34b22c1795840225c0ac9a
Devin Gaffney <itsme@devingaffney.com>
2012-03-20 09:41:15 +0000

    working on basic_histogram

--- code/analyzer/tools/basic_histogram.rb
+++ code/analyzer/tools/basic_histogram.rb
@@ -76,6 +76,7 @@ class BasicHistogram < AnalysisMetadata
         date_format = Pretty.time_interval(length, DataMapper.repository.adapter.options["adapter"])
         results = DataMapper.repository.adapter.select("select count(distinct(twitter_id)) as value,date_format(#{fs[:attribute].to_s}, '#{date_format}') as #{fs[:attribute]} from #{fs[:model].storage_name} #{Analysis.conditions_to_mysql_query(conditional)} group by date_format(#{fs[:attribute].to_s}, '#{date_format}') order by count(distinct(twitter_id)) asc limit #{limit} offset #{offset}")
         while !results.empty?
+          debugger
           graph_points = results.collect{|record| {:label => record.send(fs[:attribute].to_s), :value => record.value, :graph_id => graph.id, :curation_id => graph.curation_id}}
           GraphPoint.save_all(graph_points) if fs[:generate_graph_points]
           offset+=limit
