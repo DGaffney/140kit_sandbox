8a9f9152d93610199f0e46e31316ea15442b5272
Ian Pearce <peeinears@gmail.com>
2012-03-21 01:10:50 -0700

    useful gem was killing backtrace; fixed crazy form; redirect to researchers#new after signup

--- rails/app/controllers/sessions_controller.rb
+++ rails/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     researcher = Researcher.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Researcher.create_with_omniauth(auth)
     session[:researcher_id] = researcher.id
     if researcher.first_time
-      redirect_to edit_researcher_url(researcher), :notice => "Welcome, #{researcher.name}!"
+      redirect_to new_researcher_url(researcher), :notice => "Welcome, #{researcher.name}!"
     else
       redirect_to dashboard_url, :notice => "Hi, #{researcher.name}!"
     end
