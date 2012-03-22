d2d1bf38587761c1ff50d90adc25c61cbc59923e
Devin Gaffney <itsme@devingaffney.com>
2012-03-22 00:14:58 +0000

    rolling stone

--- code/analyzer/tools/interaction_list.rb
+++ code/analyzer/tools/interaction_list.rb
@@ -22,11 +22,7 @@ class InteractionList < AnalysisMetadata
         style = interaction.retweeted.nil? ? "mention" : "retweet"
         graph = interaction.retweeted.nil? ? mention_network : retweet_network
         edge = {:start_node => interaction.start_node, :end_node => interaction.end_node, :edge_id => interaction.edge_id, :time => interaction.time, :style => style, :analysis_metadata_id => @analysis_metadata.id, :graph_id => graph.id, :curation_id => curation.id}
-        if style == "mention"
-          mention_network << edge
-        else
-          retweet_network << edge
-        end
+        edges << edge
       end
       offset += limit
       interactions = DataMapper.repository.adapter.select("select tweets.screen_name as start_node,tweets.twitter_id as edge_id, tweets.retweeted as retweeted, tweets.created_at as time, entities.value as end_node from tweets inner join entities on tweets.twitter_id = entities.twitter_id #{Analysis.conditions_to_mysql_query(conditional).gsub("dataset_id", "entities.dataset_id")} and entities.name = 'screen_name' limit #{limit} offset #{offset}")
