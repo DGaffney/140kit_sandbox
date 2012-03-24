a1c7629f89f95006b7781e866f86b210d1c42784
Devin Gaffney <itsme@devingaffney.com>
2012-03-24 18:44:21 +0000

    WOO

--- code/analyzer/tools/geo_map.rb
+++ code/analyzer/tools/geo_map.rb
@@ -4,7 +4,7 @@ class GeoMap < AnalysisMetadata
     @analysis_metadata = AnalysisMetadata.first(:id => analysis_metadata_id)
     curation = @analysis_metadata.curation
     conditional = Analysis.curation_conditional(curation)
-    location_overview = Graph.first_or_create(:title => "location_overview", :tyle => "table", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
+    location_overview = Graph.first_or_create(:title => "location_overview", :style => "table", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
     graph_points = []
     tweet_count = Tweet.count(conditional)
     graph_points << {:label => "total_tweets", :value => tweet_count, :curation_id => curation.id, :graph_id => location_overview.id, :analysis_metadata_id => @analysis_metadata_id}
@@ -14,8 +14,8 @@ class GeoMap < AnalysisMetadata
     graph_points << {:label => "total_coordinates", :value => coordinate_count, :curation_id => curation.id, :graph_id => location_overview.id, :analysis_metadata_id => @analysis_metadata_id}
     graph_points << {:label => "proportion_geos", :value => geo_count/tweet_count.to_f, :curation_id => curation.id, :graph_id => location_overview.id, :analysis_metadata_id => @analysis_metadata_id}
     graph_points << {:label => "proportion_coordinates", :value => coordinate_count/tweet_count.to_f, :curation_id => curation.id, :graph_id => location_overview.id, :analysis_metadata_id => @analysis_metadata_id}
-    country_map = Graph.first_or_create(:title => "country_map", :tyle => "map", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
-    city_map = Graph.first_or_create(:title => "city_map", :tyle => "map", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
+    country_map = Graph.first_or_create(:title => "country_map", :style => "map", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
+    city_map = Graph.first_or_create(:title => "city_map", :style => "map", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
     offset = 0
     limit = 1000
     geos = DataMapper.repository.adapter.select("select count(*) as country_count, country from #{Analysis.conditions_to_mysql_query(conditional)} geos group by country limit #{limit} offset #{offset}")
