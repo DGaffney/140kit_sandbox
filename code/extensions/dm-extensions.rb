087df0834ce26155c341af4589e6abf249b8ed59
Devin Gaffney <itsme@devingaffney.com>
2012-03-25 14:55:45 +0100

    require zoned-time to fix fucking DST crap

--- code/extensions/dm-extensions.rb
+++ code/extensions/dm-extensions.rb
@@ -97,4 +97,52 @@ end
 
 DataMapper::Model.append_extensions(DataMapperExtensions)
 
+module DataMapper
+  class Property
+    class ZonedTime < Time
+      include DataMapper::Zone::Types
+
+      def valid?(value, negated = false)
+        response = value.kind_of?(::Time) || value.nil? || value.kind_of?(::Range)
+        negated ? !response : response
+      end
+
+      def load(value)
+        value.nil? ? nil : value
+      end
+
+      def dump(value)
+        return nil unless value
+        zone = DataMapper::Zone::Types.storage_zone
+        if value.respond_to?(:in_time_zone)
+          time_with_zone_to_primitive value.in_time_zone(zone)
+        elsif ::Range === value
+          if value.first.respond_to?(:in_time_zone) && value.last.respond_to?(:in_time_zone)
+            ((time_with_zone_to_primitive(value.first.in_time_zone(zone)))..(time_with_zone_to_primitive(value.last.in_time_zone(zone))))
+          else
+            value
+          end
+        else
+          value
+        end
+      end
+
+      def typecast(value)
+        case value
+        when ::String
+          ::Time.zone.parse(value)
+        when ::Date
+          value.to_time.in_time_zone
+        else
+          value.respond_to?(:in_time_zone) ? value.in_time_zone : value
+        end
+      end
+
+      private
+      def time_with_zone_to_primitive(value)
+        value.to_datetime.to_time
+      end
+    end
+  end
+end
 
