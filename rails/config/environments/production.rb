bbe293e470d4235b87a33805e6c947b4797d71ff
Devin Gaffney <itsme@devingaffney.com>
2012-04-05 15:27:31 +0100

    Giancarlo Dittamo

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -50,7 +50,7 @@ WWW140kit::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( google-maps-box.js jquery-tags-input.js jquery.ui.mouse.min.js jquery.ui.slider.min.js jquery.ui.widget.min.js jquery-ui-1.8.18.custom.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
