9cefa8bb706edc06b66d16ccef82a943e7d7aa68
Devin Gaffney <itsme@devingaffney.com>
2012-04-05 15:07:59 +0100

    I'm not looking for Money. Only for Exposure

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -43,7 +43,7 @@ WWW140kit::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store
+  config.cache_store = :file_store, "/media/files/cache/production"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
