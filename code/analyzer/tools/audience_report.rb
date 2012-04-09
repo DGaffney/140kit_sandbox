4a5ad37779ff3872b9b17cbd880bded6d363b8ad
Devin Gaffney <itsme@devingaffney.com>
2012-04-09 19:01:44 -0400

    add new analytic

--- code/analyzer/tools/audience_report.rb
+++ code/analyzer/tools/audience_report.rb
@@ -5,11 +5,21 @@ class AudienceReport
     curation = @analysis_metadata.curation
     conditional = Analysis.curation_conditional(curation)
     audience_overview_graph = Graph.first_or_create(:title => "audience_overview", :style => "table", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
+    top_impressioning_users = Graph.first_or_create(:title => "top_impression_generating_users", :style => "table", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
     impressions = User.sum(:followers_count, conditional)
     top_20_impressioning_users = User.all(conditional.merge({:order => :followers_count, :limit => 20, :fields => [:screen_name, :followers_count]}))
     total_retweets = Tweet.count(conditional.merge({:in_reply_to_status_id.not => nil}))
     total_replies = Tweet.count(conditional.merge({:in_reply_to_status_id => nil, :in_reply_to_screen_name.not => nil}))
     total_regulars = Tweet.count(conditional.merge({:in_reply_to_status_id => nil, :in_reply_to_screen_name => nil}))
+    graph_points = []
+    graph_points << {:label => "total_impressions", :value => impressions, :curation_id => curation.id, :analysis_metadata_id => @analysis_metadata.id, :graph_id => audience_overview_graph.id}
+    graph_points << {:label => "total_retweets", :value => total_retweets, :curation_id => curation.id, :analysis_metadata_id => @analysis_metadata.id, :graph_id => audience_overview_graph.id}
+    graph_points << {:label => "total_replies", :value => total_replies, :curation_id => curation.id, :analysis_metadata_id => @analysis_metadata.id, :graph_id => audience_overview_graph.id}
+    graph_points << {:label => "total_regulars", :value => total_regulars, :curation_id => curation.id, :analysis_metadata_id => @analysis_metadata.id, :graph_id => audience_overview_graph.id}
+    top_20_impressioning_users.each do |user|
+      graph_points << {:label => user.screen_name, :value => user.followers_count, :curation_id => curation.id, :analysis_metadata_id => @analysis_metadata.id, :graph_id => top_impressioning_users.id}
+    end
+    GraphPoint.save_all(graph_points)
     impressions_over_time = Graph.first_or_create(:title => "impressions_over_time", :style => "table", :analysis_metadata_id => @analysis_metadata.id, :curation_id => curation.id)
     first = Tweet.first(conditional.merge({:order => :created_at})).created_at
     last = Tweet.last(conditional.merge({:order => :created_at})).created_at
