c8cae2c282e0315e328797c68f22e1ba27eeaf42
Devin Gaffney <itsme@devingaffney.com>
2012-11-16 15:11:41 -0800

    mm

---
+++ rails/app/models/notification.rb
@@ -0,0 +1,24 @@
+class Notification
+  attr_accessor :twitter
+
+  def initialize(user_name="140kitRobot")
+    researcher = Researcher.first(:user_name => user_name)
+    self.twitter = TwitterOAuth::Client.new(
+      :consumer_key => Setting.first(:name => "consumer_key", :var_type => "Site Consumer Key").value,
+      :consumer_secret => Setting.first(:name => "consumer_secret", :var_type => "Site Consumer Secret").value,
+      :token => researcher.oauth_token,
+      :secret => researcher.oauth_token_secret
+    )
+    self.twitter.authorized?
+  end
+  
+  def self.post(content)
+    if content.length < 140
+      n = Notification.new
+      n.twitter.update(content)
+      return true
+    else
+      return false
+    end
+  end
+end
