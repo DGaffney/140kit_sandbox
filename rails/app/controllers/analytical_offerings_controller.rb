2e2654e40c50cea5f6a62b492d1742beef93f1d1
Devin Gaffney <itsme@devingaffney.com>
2012-11-16 15:55:31 -0800

    quick

--- rails/app/controllers/analytical_offerings_controller.rb
+++ rails/app/controllers/analytical_offerings_controller.rb
@@ -62,6 +62,7 @@ class AnalyticalOfferingsController < ApplicationController
   end
 
   def add
+    current_user
     @curation = Curation.find(params[:curation_id])
     @analytical_offering = AnalyticalOffering.find(params[:id])
     @analysis_metadata = AnalysisMetadata.new(:curation_id => params[:curation_id], :analytical_offering_id => params[:id], :ready => false)
